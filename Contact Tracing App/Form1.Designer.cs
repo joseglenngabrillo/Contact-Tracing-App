538b8c1c043a7ca4ce859777cc34e5774e915d95
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-29 11:14:38 +0800

    Renamed Text Property into Contact Tracing App.

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace Contact_Tracing_App
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Contact Tracing App";
             this.ResumeLayout(false);
 
         }

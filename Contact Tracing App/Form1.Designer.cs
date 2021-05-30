b8f52f1862c80d606ccc869aee76d28cb6734910
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-30 18:16:06 +0800

    Renamed label1 into FirstName.

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -29,24 +29,24 @@ namespace Contact_Tracing_App
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // FirstName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(26, 13);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(57, 13);
+            this.FirstName.TabIndex = 0;
+            this.FirstName.Text = "First Name";
             // 
             // Contact_Tracing_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FirstName);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Contact_Tracing_App";
             this.ShowIcon = false;
@@ -59,7 +59,7 @@ namespace Contact_Tracing_App
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FirstName;
     }
 }
 

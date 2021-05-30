bfc19fcd4adb2ac9b36463a295582ebf2e91f557
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-30 18:18:57 +0800

    Insert third label.

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Contact_Tracing_App
         {
             this.FirstName = new System.Windows.Forms.Label();
             this.MiddleName = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FirstName
@@ -51,11 +52,21 @@ namespace Contact_Tracing_App
             this.MiddleName.TabIndex = 1;
             this.MiddleName.Text = "Middle Name";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
             // Contact_Tracing_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.MiddleName);
             this.Controls.Add(this.FirstName);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -72,6 +83,7 @@ namespace Contact_Tracing_App
 
         private System.Windows.Forms.Label FirstName;
         private System.Windows.Forms.Label MiddleName;
+        private System.Windows.Forms.Label label3;
     }
 }
 

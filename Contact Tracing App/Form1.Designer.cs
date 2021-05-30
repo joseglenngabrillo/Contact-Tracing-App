5f937d61ab0eba8da2cf30df0e7014fc8df52b9e
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-05-30 18:18:04 +0800

    Renamed label2 into MiddleName.

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -30,7 +30,7 @@ namespace Contact_Tracing_App
         private void InitializeComponent()
         {
             this.FirstName = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.MiddleName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // FirstName
@@ -42,21 +42,21 @@ namespace Contact_Tracing_App
             this.FirstName.TabIndex = 0;
             this.FirstName.Text = "First Name";
             // 
-            // label1
+            // MiddleName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.MiddleName.AutoSize = true;
+            this.MiddleName.Location = new System.Drawing.Point(29, 44);
+            this.MiddleName.Name = "MiddleName";
+            this.MiddleName.Size = new System.Drawing.Size(69, 13);
+            this.MiddleName.TabIndex = 1;
+            this.MiddleName.Text = "Middle Name";
             // 
             // Contact_Tracing_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MiddleName);
             this.Controls.Add(this.FirstName);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Contact_Tracing_App";
@@ -71,7 +71,7 @@ namespace Contact_Tracing_App
         #endregion
 
         private System.Windows.Forms.Label FirstName;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label MiddleName;
     }
 }
 

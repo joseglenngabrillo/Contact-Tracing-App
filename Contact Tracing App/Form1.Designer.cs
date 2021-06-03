a1ff6e432a42e36640691284167dbc649e62f8d6
Jose Glenn Gabrillo Jr <joseglenncgabrillojr@iskolarngbayan.pup.edu.ph>
2021-06-03 22:31:23 +0800

    Insert button2.

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -44,6 +44,7 @@ namespace Contact_Tracing_App
             this.Email = new System.Windows.Forms.Label();
             this.EmailBox7 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // FirstName
@@ -171,11 +172,21 @@ namespace Contact_Tracing_App
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 185);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Contact_Tracing_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(810, 498);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.EmailBox7);
             this.Controls.Add(this.Email);
@@ -218,6 +229,7 @@ namespace Contact_Tracing_App
         private System.Windows.Forms.Label Email;
         private System.Windows.Forms.TextBox EmailBox7;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
